25015507fc2615fd642d75b8b21c786e785936e5
Ali Lari <ali.larica@icloud.com>
2026-02-08 21:36:38 +0400

    fix bug

--- components/founders.tsx
+++ components/founders.tsx
@@ -31,7 +31,7 @@ export function Founders() {
           For gamers.
         </p>
 
-        <div className={"grid grid-cols-1 md:grid-cols-2 gap-4 sm:gap-6 md:gap-8 ${founders.length === 1 ? "justify-center" : """}>
+        <div c  className={`grid grid-cols-1 md:grid-cols-2 gap-8 ${founders.length === 1 ? "justify-center" : ""}`}>
           {founders.map((founder, index) => (
             <div
               key={index}
